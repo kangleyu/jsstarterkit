041eccb85e26b7cbe78380d422fccf585056755d
Tom Yu <kangleyu@gmail.com>
2017-02-26 17:32:48 +0800

    add eslint for monitoring source code

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk'; // ES6 feature
 
-console.log(chalk.green("Starting app in dev mode..."));
+console.log(chalk.green("Starting app in dev mode...")); // eslint-disable-line no-console
