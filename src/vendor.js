17fcb60132553f69c6d1dcaba205a94f47261fde
Tom Yu <kangleyu@gmail.com>
2017-02-27 14:54:00 +0800

    bundle split into diff files, and we put hash code in output file name

---
+++ src/vendor.js
@@ -0,0 +1,8 @@
+/**
+ * This file contains references to the vendor libraries
+ * we're using in this project.
+ */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
 
 export default {
   debug: true,
@@ -12,19 +13,27 @@ export default {
       'node_modules'
     ]
   },
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    // Hash the files using MD5 so that their names change when the content changes
+    new WebpackMd5Hash(),
+    // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they're cached separately
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
     // create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
       template: 'src/index.html',
